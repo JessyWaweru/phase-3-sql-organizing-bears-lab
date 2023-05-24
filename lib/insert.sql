2f929b80231e5aa3f4800f09abcdd0c0cebad2c4
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-05-24 13:05:05 +0300

    assignment done

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears(
+name,age,sex,color,temperament,alive
+)
+VALUES
+('Mr.Chocolate',1,'M','black','happy',true),
+('Rowdy',2,'F','black','sad',true),
+('Tabitha',3,'F','brown','sad',true),
+('Sergeant Brown',3,'M','brown','happy',true),
+('Melissa',2,'F','white','happy',true),
+('Grinch',4,'M','black','angry',true),
+('Wendy',4,'F','white','happy',true),
+(null,4,'F','white','happy',true)
+
